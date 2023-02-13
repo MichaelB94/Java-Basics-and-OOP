1db7f2274f422ededcd6e6470c0160119e191373
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-02-13 16:22:54 +0100

    completed
    non capisco perchè mi dia errore la sintassi del quarto esercizio già da prima di svolgerlo

--- javabasics/_9/Main.java
+++ javabasics/_9/Main.java
@@ -14,7 +14,7 @@ public class Main {
      */
     private static void exercise1() {
         System.out.println("\nExercise 1:");
-        int porridgeHeatInCentigrade = 30; // <--- Change this value
+        int porridgeHeatInCentigrade = 60; // <--- Change this value
 
         if (porridgeHeatInCentigrade < 60) {
             System.out.println("Too cold!");
@@ -39,6 +39,11 @@ public class Main {
         double lunchCost = 5.0;
 
         // Your if statement here
+        if(remainingMoney>=lunchCost){
+            System.out.println("Lunch time!");
+        } else {
+            System.out.println("You go hungry today");
+        }
     }
 
     /**
@@ -57,25 +62,45 @@ public class Main {
         System.out.println("\nExercise 3:");
         double ourDouble = 3.5;
         //Write your code here
+        if (ourDouble > 2.5) {
+            System.out.println("Greater than");
+        } else {
+            ourDouble++;
+            if (ourDouble < 2.5) {
+                System.out.println("Still less than");
+            } else {
+                System.out.println("Now greater than");
+            }
+        }
+
+
+        /**
+         * 4: Create an if statement with 4 outcomes, (1 if, 2 else if and an else)
+         *
+         *    That checks the following:
+         *    1 - if our age is greater than OR EQUAL TO (>=) 65 print: "Retired"
+         *    2 - if our age is greater than OR EQUAL TO 35 but less than 65 print "Experienced Worker"
+         *    3 - if our age is greater than OR EQUAL TO 18 but less than 35 print "New Worker"
+         *    4 - if our age is less than 18 print: "School age"
+         *
+         *    Remember, the order is very important!
+         */
+        private static void exercise4() {
+            System.out.println("\nExercise 4:");
+
+            int employeeAge = 30;
+
+            //Write your if statement here
+        if(employeeAge>= 65) {
+            System.out.println("Retired");
+        }else if(employeeAge>=35){
+            System.out.println("Experienced Worker");
+        }else if(employeeAge>=18){
+            System.out.println("New Worker");
+        } else {
+            System.out.println("School Age");
+        }
+
+            }
+        }
     }
-
-
-    /**
-     * 4: Create an if statement with 4 outcomes, (1 if, 2 else if and an else)
-     *
-     *    That checks the following:
-     *    1 - if our age is greater than OR EQUAL TO (>=) 65 print: "Retired"
-     *    2 - if our age is greater than OR EQUAL TO 35 but less than 65 print "Experienced Worker"
-     *    3 - if our age is greater than OR EQUAL TO 18 but less than 35 print "New Worker"
-     *    4 - if our age is less than 18 print: "School age"
-     *
-     *    Remember, the order is very important!
-     */
-    private static void exercise4() {
-        System.out.println("\nExercise 4:");
-
-        int employeeAge = 30;
-
-        //Write your if statement here
-    }
-}
