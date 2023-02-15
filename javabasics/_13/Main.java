7a3f71d996aca47c5e7d844240f9ce9f41b2d4ce
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-02-15 17:58:08 +0100

    completed

--- javabasics/_13/Main.java
+++ javabasics/_13/Main.java
@@ -23,6 +23,23 @@ public class Main {
         ArrayList<Integer> numbers = new ArrayList<>();
 
         //Write your code here
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(8);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(4);
+        numbers.add(7);
+
+        System.out.println(numbers.get(0));
+
+        numbers.remove(0);
+        numbers.remove(0);
+        numbers.remove(0);
+
+        System.out.println(numbers.get(0));
+
     }
 
     /**
@@ -38,8 +55,18 @@ public class Main {
      * menu item correspond
      */
     public static void exercise2() {
-        System.out.println("\nExercise 1:");
+        System.out.println("\nExercise 2:");
         //Write your code here
+        ArrayList<String> menuItems = new ArrayList<>();
+        menuItems.add("hamburger");
+        menuItems.add("fries");
+        menuItems.add("pizza");
+        ArrayList<Double> menuPrices = new ArrayList<>();
+        menuPrices.add(4.99);
+        menuPrices.add(2.49);
+        menuPrices.add(7.99);
+        System.out.println(menuItems.get(0)+" cost " + menuPrices.get(0));
+
     }
 
     /**
@@ -56,6 +83,9 @@ public class Main {
     public static void exercise3() {
         System.out.println("\nExercise 3:");
         int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
+        int sum = items[0] + items[1] + items[2] + items[3] + items[4] + items[5] + items[6] +
+                items[7] + items [8] + items[9];
+        System.out.println(sum);
 
         //Write your code here
     }
